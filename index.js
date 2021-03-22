7b5bed6e294ae3b2f32da293d6e8f897f3b684b1
PhpMyNewdun <30565751+phpmynewdun@users.noreply.github.com>
2021-03-22 21:23:49 +0100

    Discord Rich Presence
    Rich presence discord pour la FRC

---
+++ index.js
@@ -0,0 +1,21 @@
+var rpc = require("discord-rpc")
+const client = new rpc.Client({ 
+	transport: 'ipc' 
+})
+client.on('ready', () => {
+client.request('SET_ACTIVITY', {
+pid: process.pid,
+activity : {
+details : "🌹 FrenchRobloxCommunity 🌹",
+state: "🌴✨ Rejoins nous ✨🌴",
+assets : {
+large_image : "frc",
+large_text : "🌹 FrenchRobloxCommunity 🌹"
+},
+buttons : [{label : "Rejoins nous ! ❤️" , url : "https://discord.gg/YH8Qbjr2sJ"},{label : "Vote pour nous 💎", url : "https://serveur-prive.net/discord/french-roblox-community-3293"}]
+}
+})
+})
+client.login({ 
+	clientId : "820236976439164949" 
+}).catch(console.error);
