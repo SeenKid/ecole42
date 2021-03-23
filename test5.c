ff42b135d39e54980418e3b0d24ed858b5db3a47
PhpMyNewdun <30565751+phpmynewdun@users.noreply.github.com>
2021-03-24 00:57:04 +0100

    Update test5.c

--- test5.c
+++ test5.c
@@ -4,7 +4,7 @@ int main(void)
 {
 	int ageUtilisateur = 0;
 
-	printf("Quel âge avez vous ?");
+	printf("Quel âge avez vous ?\n");
 	scanf("%d", &ageUtilisateur);
 
 	printf("vous avez %d ans.\n", ageUtilisateur);
