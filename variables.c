240a906af6042169862efb015d3d59be0d6a41c2
PhpMyNewdun <30565751+phpmynewdun@users.noreply.github.com>
2021-06-02 20:27:12 +0200

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,10 @@
+int main(int argc, char *argv[])
+{
+  int age = 0;
+  
+  printf("Quel age avez-vous ? ");
+  scanf("%d", &age); 
+  printf("Ah ! Vous avez donc %d ans !\n\n", age);
+       
+  return 0;
+}
---
+++ variables2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+	double resultat = 0;
+
+	resultat = 5.0 / 3.0;
+
+	printf("5 / 3 = %lf", resultat);
+}
+
+/*
+
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+	int resultat = 0;
+
+	resultat = 5 + 3;
+
+	printf("5 + 3 = %d", resultat);
+}
