5cf798c23de519c50ec1b50386391c2277b70a50
PhpMyNewdun <30565751+phpmynewdun@users.noreply.github.com>
2021-06-02 23:43:00 +0200

    Add files via upload

---
+++ while.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int
+main (int argc, char **argv)
+{
+
+  int nombreEntre = 0;
+
+  while (nombreEntre != 47)
+    {
+      printf ("Tapez le nombre 47 ! ");
+      scanf ("%d", &nombreEntre);
+    }
+}
