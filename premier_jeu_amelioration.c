6a5cc310f943e1d2433a714076e09de2298a4b61
PhpMyNewdun <30565751+phpmynewdun@users.noreply.github.com>
2021-06-03 11:08:03 +0200

    Add files via upload

--- premier_jeu_amelioration.c
+++ premier_jeu_amelioration.c
@@ -2,29 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main ( int argc, char** argv )
+int
+main (int argc, char **argv)
 {
-    int nombreMystere = 0, nombreEntre = 0;
-    int nombreCoups = 0;
-    const int MAX = 100, MIN = 1;
+  int nombreMystere = 0, nombreEntre = 0;
+  int tentatives = 0;
+  const int MAX = 100, MIN = 1;
 
-    // Génération du nombre aléatoire
+  srand (time (NULL));
+  nombreMystere = (rand () % (MAX - MIN + 1)) + MIN;
 
-    srand(time(NULL));
-    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+  do
+    {
+      printf ("Quel est le nombre ? ");
+      scanf ("%d", &nombreEntre);
+      tentatives++;
 
-    do {
-        printf("Quel est le nombre ? ");
-        scanf("%d", &nombreEntre);
-        nombreCoups++;
+      if (nombreMystere > nombreEntre)
+    printf ("C'est plus !\n\n");
+      else if (nombreMystere < nombreEntre)
+    printf ("C'est moins !\n\n");
+      else
+    printf
+      ("Bravo, vous avez trouve le nombre mystere en %d tentatives !!!\n\n Made by TweekLG",
+       tentatives);
+    }
+  while (nombreEntre != nombreMystere);
 
-        if (nombreMystere > nombreEntre)
-            printf("C'est plus !\n\n");
-        else if (nombreMystere < nombreEntre)
-            printf("C'est moins !\n\n");
-        else
-            printf ("Bravo, vous avez trouve le nombre mystere en %d tentatives !!!\n\n Made by TweekLG", nombreCoups);
-    } while (nombreEntre != nombreMystere);
-    
-    return 0;
+  return 0;
 }
