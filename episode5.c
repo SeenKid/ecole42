04ce7d356b94ff6d276b6a566e3707062ba2d574
PhpMyNewdun <30565751+phpmynewdun@users.noreply.github.com>
2021-06-05 17:53:45 +0200

    formationvideo #5

---
+++ episode5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int calcul = 6 + 4;
+	int calcul2 = 6 * 4;
+	int calcul3 = 20 / 4;
+	int calcul4 = 22 % 4;
+
+	printf("6 + 4 = %d\n", calcul);
+	printf("6 * 4 = %d\n", calcul2);
+	printf("6 / 4 = %d\n", calcul3);
+	printf("6 % 4 = %d\n", calcul4);
+
+	return 0;
+}
