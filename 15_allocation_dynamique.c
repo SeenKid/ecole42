27f13c0101c908095c1b2d54ff15514de8c8c996
PhpMyNewdun <30565751+phpmynewdun@users.noreply.github.com>
2021-06-16 23:32:57 +0200

    Add files via upload

---
+++ 15_allocation_dynamique.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) 
+{
+
+	int nombreJoueurs = 0;
+	int *liste_joueurs = NULL;
+	int i;
+
+	printf("Combien de joueurs vont jouer avec vous ?");
+	scanf("%d" &nombreJoueurs);
+
+	liste_joueurs = malloc(sizeof(int) * nombreJoueurs);
+	//liste_joueurs = calloc(nombreJoueurs, sizeof(int));
+
+	if(liste_joueurs == NULL)
+		exit(1);
+
+	for(i = 0 ; i < nombreJoueurs ; i++) 
+	{
+		printf("Joueur %d --> numéro %d\n", i + 1, i * 3);
+		liste_joueurs[i] = i * 3;
+	}
+
+	for(i = 0 ; i < nombreJoueurs ; i++) 
+	{
+		printf("[%d] ", liste_joueurs[i]);
+	}
+
+	/* *********************************************** */
+
+	free(liste_joueurs);
+
+	return 0;
+}
